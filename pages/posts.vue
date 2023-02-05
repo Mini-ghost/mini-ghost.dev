16471c271892c23466c9e6995982c5748090e8af
Alex Liu <dsa1314@gmail.com>
2023-02-05 12:44:38 +0800

    feat: group posts by creation year

--- pages/posts.vue
+++ pages/posts.vue
@@ -1,25 +1,74 @@
-
-
 <script setup lang="ts">
-const posts = await(await queryContent('/posts').find())
-  .filter(item => !!item._path)
-  .map(item => ({
-    path: item._path,
-    title: item.title,
-  }));
+interface Post {
+  _path: string;
+  title: string;
+  created: string;
+}
+
+const { data: posts } = await useAsyncData(
+  'QUERY_POSTS',
+  () =>
+    queryContent('/posts/')
+      .where({ _partial: false })
+      .only(['_path', 'title', 'created'])
+      .sort({ created: -1 })
+      .find() as Promise<Post[]>,
+  {
+    transform(result) {
+      const posts: { year: string; posts: Post[] }[] = [];
+
+      let group: Post[] = [];
+      let year: string | null = null;
+
+      result.forEach(item => {
+        let current: string;
+        if (
+          (current = item.created.slice(0, 4)) !== year &&
+          typeof year === 'string'
+        ) {
+          posts.push({
+            year,
+            posts: group,
+          });
+
+          group = [];
+        }
+
+        year = current;
+        group.push(item);
+      });
+
+      if (typeof year === 'string' && group.length) {
+        posts.push({
+          year,
+          posts: group,
+        });
+      }
+
+      return posts;
+    },
+  }
+);
 </script>
 
 <template>
   <div>
-    <ol>
-      <li
-        v-for="post in posts"
-        :key="post.path"
-      >
-        <NuxtLink :to="post.path">
-          {{ post.title }}
-        </NuxtLink>
-      </li>
-    </ol>
+    <div
+      v-for="group in posts"
+      :key="group.year"
+    >
+      <div>{{ group.year }}</div>
+      <ol>
+        <li
+          v-for="post in group.posts"
+          :key="post._path"
+        >
+          <NuxtLink :to="post._path">
+            {{ post.title }}
+          </NuxtLink>
+          <time :datetime="post.created">{{ post.created }}</time>
+        </li>
+      </ol>
+    </div>
   </div>
 </template>
