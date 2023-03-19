e22156f1628d47d07105f253b0ac6ed54adb1b78
Alex Liu <dsa1314@gmail.com>
2023-03-19 13:51:58 +0800

    chore: adjust ui style

--- pages/posts/[...prose].vue
+++ pages/posts/[...prose].vue
@@ -40,64 +40,89 @@ import format from '@/helper/format';
 <style lang="scss">
 .prose {
   --at-apply: leading-loose;
-}
 
-.prose :is(h1, h2, h3) {
-  --at-apply: font-bold;
-  --at-apply: mt-8 mb-4;
-}
+  :is(h1, h2, h3) {
+    --at-apply: font-bold;
+    --at-apply: mt-8 mb-4;
+  }
 
-.prose h2 {
-  --at-apply: text-3xl;
-}
+  h2 {
+    --at-apply: text-3xl;
+  }
 
-.prose h3 {
-  --at-apply: text-2xl;
-}
+  h3 {
+    --at-apply: text-2xl;
+  }
 
-.prose :is(p, pre) {
-  --at-apply: my-4;
-}
+  :is(p, pre) {
+    --at-apply: my-4;
+  }
 
-.prose p {
-  --at-apply: my-6;
-}
+  p {
+    --at-apply: my-6;
+  }
 
-.prose :not(h1, h2, h3) > a {
-  --at-apply: underline decoration-1 decoration-dashed underline-offset-6;
-  --at-apply: hover:text-[#FFAC11];
-}
+  :not(h1, h2, h3) > a {
+    --at-apply: underline decoration-1 decoration-dashed underline-offset-6;
+    --at-apply: hover:text-[#FFAC11];
+  }
 
-.prose pre {
-  --at-apply: text-sm;
-  --at-apply: bg-[var(--bg-code-block)];
-  --at-apply: border border-solid border-gray/10;
-  --at-apply: p-4;
-  --at-apply: rounded-2 overflow-x-auto;
-}
+  pre {
+    --at-apply: text-sm;
+    --at-apply: bg-[var(--bg-code-block)];
+    --at-apply: border border-solid border-gray/10;
+    --at-apply: p-4;
+    --at-apply: rounded-2 overflow-x-auto;
+  }
 
-.prose :not(pre) > code {
-  --at-apply: text-[0.785rem];
-  --at-apply: text-[var(--c-text-code)];
-  --at-apply: bg-[var(--bg-code-inline)];
-  --at-apply: py-1 px-1.5;
-  --at-apply: rounded;
-}
+  :not(pre) > code {
+    --at-apply: text-[var(--c-text-code)];
+    --at-apply: bg-[var(--bg-code-inline)];
+    --at-apply: py-1 px-1.5;
+    --at-apply: rounded;
+  }
 
-.prose :is(ul, ol) {
-  --at-apply: ps-5;
-}
+  :not(pre, h1, h2, h3, h4, h5, h6) > code {
+    --at-apply: text-[0.785rem];
+  }
 
-.prose ul {
-  --at-apply: list-disc;
-}
+  :is(ul, ol) {
+    --at-apply: ps-5;
+  }
 
-.prose ol {
-  --at-apply: list-decimal;
-}
+  ul {
+    --at-apply: list-disc;
+  }
 
-.prose li {
-  --at-apply: my-2;
-  --at-apply: marker:text-gray/60;
+  ol {
+    --at-apply: list-decimal;
+  }
+
+  li {
+    --at-apply: my-2;
+    --at-apply: marker:text-gray/60;
+  }
+
+  table {
+    --at-apply: block;
+    --at-apply: border-collapse;
+    --at-apply: overflow-x-auto;
+    --at-apply: my-5;
+  }
+
+  th {
+    --at-apply: text-sm;
+    --at-apply: font-bold;
+    --at-apply: bg-[var(--bg-soft)]
+  }
+
+  tr {
+    --at-apply: border-t border-[var(--c-divider)];
+  }
+
+  :is(th, td) {
+    --at-apply: border border-[var(--c-divider)];
+    --at-apply: py-2 px-4
+  }
 }
 </style>
