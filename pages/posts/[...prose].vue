ae934d6b0ce737f216ef727822fb1cf8149fbde8
Alex Liu <dsa1314@gmail.com>
2023-04-03 11:44:57 +0800

    feat: add previous post and next post link

--- pages/posts/[...prose].vue
+++ pages/posts/[...prose].vue
@@ -1,5 +1,21 @@
 <script setup lang="ts">
+import { withoutTrailingSlash } from 'ufo';
+
 import format from '@/helper/format';
+
+const route = useRoute();
+const path = toRef(route, 'path');
+
+// TODO 整合到 <ContentDoc> 裡面
+const { data: surround } = await useAsyncData(
+  `CONTENT_SURROUND:${path.value}`,
+  () =>
+    queryContent('/posts/')
+      .where({ _partial: false })
+      .only(['_path', 'title'])
+      .sort({ created: 1 })
+      .findSurround(withoutTrailingSlash(path.value))
+);
 </script>
 
 <template>
@@ -10,7 +26,7 @@ import format from '@/helper/format';
         :description="doc.description"
         :image="doc.image"
       />
-      <article class="prose max-w-21cm w-11/12 mx-auto pb-16 lg:pb-32">
+      <article class="prose max-w-21cm w-11/12 mx-auto">
         <header>
           <h1 class="mt-0 text-[1.75rem] lg:text-[2.25rem] leading-relaxed">
             <NuxtLink :to="doc._path">
@@ -34,6 +50,43 @@ import format from '@/helper/format';
           :excerpt="excerpt"
         />
       </article>
+      <div class="max-w-21cm w-11/12 mx-auto mt-16 pb-16 lg:pb-32">
+        <div class="grid grid-cols-2 gap-x-4 border-t border-white border-opacity-20 pt-8">
+          <template v-if="surround && surround![0]">
+            <NuxtLink
+              :to="surround![0]._path"
+              class="text-start opacity-60 hover:opacity-100 transition-opacity"
+            >
+              <span class="opacity-60">previous</span><br>
+              {{ surround![0].title }}
+            </NuxtLink>
+          </template>
+
+
+          <template v-if="surround && surround![1]">
+            <NuxtLink
+              :to="surround![1]._path"
+              class="text-end opacity-60 hover:opacity-100 transition-opacity"
+            >
+              <span class="opacity-60">next</span><br>
+              {{ surround![1].title }}
+            </NuxtLink>
+          </template>
+        </div>
+      </div>
+    </template>
+    <template #empty>
+      <div class="max-w-21cm w-11/12 mx-auto pb-16 lg:pb-32">
+        <span>
+          NOT FONT | 
+        </span>
+        <NuxtLink
+          to="/posts"
+          class="font-bold"
+        >
+          cd..
+        </NuxtLink>
+      </div>
     </template>
   </ContentDoc>
 </template>
