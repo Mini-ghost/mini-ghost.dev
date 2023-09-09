a29ea758d8a1dddb22c22e5f6d181c76d6d0ca7b
Alex Liu <dsa1314@gmail.com>
2023-09-09 15:45:39 +0800

    feat: delay initial google analytics

--- plugins/google-analytics.client.ts
+++ plugins/google-analytics.client.ts
@@ -4,26 +4,59 @@ function gtag(..._args: any[]) {
   (window as any).dataLayer.push(arguments);
 }
 
-export default defineNuxtPlugin(() => {
+const INITIAL_EVENT = [
+  'mousemove',
+  'scroll',
+  'keydown',
+  'click',
+  'touchstart',
+  'wheel',
+];
+
+export default defineNuxtPlugin(nuxtApp => {
   // 如果是在開發中則跳過
   if (process.env.NODE_ENV !== 'production') return;
 
   const { googleTagId } = useRuntimeConfig().public;
 
-  onNuxtReady(() => {
-    useHead({
+  const initialed = ref(false);
+
+  useHead(() => {
+    if (!initialed.value) return {};
+
+    return {
       script: [
         {
           async: true,
           src: `https://www.googletagmanager.com/gtag/js?id=${googleTagId}`,
         },
       ],
-    });
-
-    // @ts-expect-error: `dataLayer` is not defined
-    window.dataLayer = window.dataLayer || [];
-
-    gtag('js', new Date());
-    gtag('config', googleTagId);
+    };
   });
+
+  nuxtApp.hooks.hook('app:created', () => {
+    const controller = new AbortController();
+
+    const handler = () => {
+      initialed.value = true;
+      INITIAL_EVENT.forEach(name => {
+        window.removeEventListener(name, handler);
+      });
+    };
+
+    INITIAL_EVENT.forEach(event => {
+      window.addEventListener(event, handler, {
+        capture: true,
+        once: true,
+        passive: true,
+        signal: controller.signal,
+      });
+    });
+  });
+
+  // @ts-expect-error: `dataLayer` is not defined
+  window.dataLayer = window.dataLayer || [];
+
+  gtag('js', new Date());
+  gtag('config', googleTagId);
 });
